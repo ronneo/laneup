5413dc34a674f64110b625c884d3616ab70e5ac1
ronneo <ron.neo@gmail.com>
2023-12-11 13:33:29 +0800

    Restructure app.js to allow testing

---
+++ api/src/server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+import wsconn from './ws/connections.js'
+import { WebSocketServer } from 'ws';
+import apiRoutes from './routes/api.js'
+import authRoutes from './routes/auth.js'
+import manageRoutes from './routes/manage.js'
+
+dotenv.config()
+const port = process.env.PORT
+
+//Start websocket server
+const wss = new WebSocketServer({ port: process.env.PUBLIC_WS_PORT })
+wss.on('connection', wsconn.initialConnection)
+apiRoutes.wsconn = wsconn
+
+const app = express()
+app.use(cors({origin: '*'}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/api', apiRoutes.routes)
+app.use('/auth', authRoutes)
+app.use('/manage', manageRoutes)
+
+try {
+    const {handler} = await import('../fe/handler.js')
+    app.use(handler)
+}
+catch(err) {
+    console.log('Unable to load frontend, operating as an API server')
+}
+
+export default app;
